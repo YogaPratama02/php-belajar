15d23b610b001c5f3b8166970d3b7e3c8413909d
YogaPratama02 <yogapratama7212@gmail.com>
2020-03-09 00:31:24 +0700

    file php dasar

---
+++ pertemuan2/index.php
@@ -0,0 +1,44 @@
+<?php 
+// pertemuan 2 - PHP dasar
+// sintaks php
+
+// standar output
+// echo, print
+// print_r
+// var_dump, melihat isi dari variabel
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam sintaks
+// variabel dan tipe data
+// variabel untuk menampung sebuah nilai
+// variabel tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Yoga";
+// penggabungan string / concatenation / 
+//  .
+//  assigment
+//  =, +=, _=, *=, /=, %=, .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+// $nama .= "Yoga";
+// $nama .= " ";
+// $nama .= "Pratama";
+// echo $nama;
+// perbandingan
+// <, >, <=, >=, ==
+ var_dump(1 == 1)
+// $nama_depan = "Yoga";
+// $nama_belakang = "Pratama";
+// echo $nama_depan . " " .$nama_belakang
+ ?>
+ <!-- <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Belajar PHP</title>
+ </head>
+ <body>
+ 	<h1>Halo, Selamat Datang <?php echo $nama ; ?></h1>
+ 	<p><?php echo "ini paragraf"; ?></p>
+ </body>
+ </html> -->
